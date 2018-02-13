da7ef88989db0fbfb3c21321cee5004e77fa3cb2
SportCrypt <support@sportcrypt.com>
2018-02-13 06:05:01 -0500

    factor match signing logic into scUtil so sign finalization script doesn't need to create SportCrypt instance

--- scripts/signFinalizationMessage.js
+++ scripts/signFinalizationMessage.js
@@ -1,11 +1,6 @@
 "use strict";
 
-const Web3 = require('web3');
-const SportCrypt = require("../lib/SportCrypt");
-
-const web3 = new Web3();
-web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
-
+const scUtil = require("../lib/scUtil");
 
 let privateKey = process.env.ADMIN_PRIVATE_KEY;
 let contractAddr = process.argv[2];
@@ -17,10 +12,6 @@ if (!contractAddr) throw("must pass in contractAddr");
 if (!matchId) throw("must pass in matchId");
 if (finalPrice === undefined) throw("must pass in finalPrice");
 
-
-let inst = new SportCrypt(web3);
-inst.attach("0x0000000000000000000000000000000000000000", contractAddr);
-
-let signed = inst.signFinalizationMessage(privateKey, matchId, parseInt(finalPrice));
+let signed = scUtil.buildFinalizationMessage(contractAddr, privateKey, matchId, parseInt(finalPrice));
 
 console.log(JSON.stringify(signed));
